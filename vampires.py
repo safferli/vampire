3d2282a6cfafa01555e8f9308e94ef43f7ad1ec8
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-06-03 15:10:36 +0200

    first working version of db with PCs included

--- vampires.py
+++ vampires.py
@@ -2,7 +2,7 @@
 
 #%%
 import sqlite3 as lite
-import sys
+# import sys
 import os
 
 #%%
@@ -23,7 +23,36 @@ with conn:
 
 #%%
 
+# (8): ID, nickname, firstnames, lastname, clan, covenant, bloodline, misc
+vamps = [
+    # PCs are single-digit (hopefully...)
+    (1, None, "Hauke", "Hinnrichs", "Nosferatu", "Ordo Dracul", None, None),
+    (2, None, "Valeria", "von Vyborg", "Ventrue", "Invictus", None, None),
+    (3, "Capt'n Ahab", "Sven", "Unterberg", "Mekhet", "Ordo Dracul", None, None),
+    (4, "Thomas", "Thomas", "Du Bois", "Gangrel", None, None, None),
+    (5, None, None, None, None, None, None, None),
+    (6, None, None, None, None, None, None, None),
+    (7, None, None, None, None, None, None, None),
+    (8, None, None, None, None, None, None, None),
+    (9, None, None, None, None, None, None, None),
+    # NPCs start here, IDs are auto-incremented
+    (None, None, "Werner", "Rosenberg", "Daeva", "Ordo Dracul", None, None)
+    ]
 
+# generate vampire db
+with conn:
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS Vampires")
+    cur.execute("CREATE TABLE Vampires(Id INTEGER PRIMARY KEY, Nickname TEXT, Firstname TEXT, Lastname TEXT, Clan TEXT, Covenant  TEXT, Bloodline TEXT, Misc TEXT)")
+    cur.executemany("INSERT INTO Vampires VALUES(?, ?, ?, ?, ?, ?, ?, ?)", vamps)    
     
+# examine db
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Vampires")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-
+# http://www.pythoncentral.io/introduction-to-sqlite-in-python/
+# http://zetcode.com/db/sqlitepythontutorial/ (python2)

7d9a66e074b9c3a0c37b9d256af4814e37ee5050
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-06-03 14:33:07 +0200

    clean version of sqlite connection

--- vampires.py
+++ vampires.py
@@ -9,35 +9,21 @@ import os
 os.getcwd()
 os.chdir("D:\\github\\vampire")
 
-#%%
 conn = lite.connect("vamps.db")
 
+#%%
+# with takes care of connection closing, and error handling already!
+# with also auto-commits changes to the db
 with conn:
     cur = conn.cursor()
     cur.execute("SELECT SQLITE_VERSION()")
     data = cur.fetchone()
     
-    print("SQLite version:", data)
-
+    print("SQLite version: %s" % data)
 
 #%%
-con = None
 
-try:
-    con = lite.connect('test.db')
-    cur = con.cursor()
 
-    cur.execute('SELECT SQLITE_VERSION()')
-
-    data = cur.fetchone()
-
-    print("SQLite Version: %s", data)
-
-except lite.Error, e:
-    print("Error %s:", %e.args[0])
-    sys.exit(1)
     
-finally:
-    
-    if con:
-        con.close()
+
+

7cc457048acbb4b1ca76d1ec77c5506b93c0ed4f
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-07-16 15:32:02 +0200

    added connections DB

---
+++ connections.py
@@ -0,0 +1,9 @@
+# list of connections between Vampires
+# for IDs, see vampy.py (or vampires_dus.db)
+# (8): connected-to ID, connected-from ID, bi-directional, connection type, connection details, start date, end date, misc
+connections = [
+    (1, 2, 1, "coterie", None, None, None, None),# Hauke and Valeria
+    (1, 3, 1, "coterie", None, None, None, None),# Hauke and Sven
+    (1, 4, 1, "coterie", None, None, None, None)# Hauke and Fred
+    #(, , 0, "", None, None, None, None)
+]
--- vampires.py
+++ vampires.py
@@ -9,7 +9,7 @@ import os
 os.getcwd()
 os.chdir("D:\\github\\vampire")
 
-conn = lite.connect("vamps.db")
+conn = lite.connect("vampires_dus.db")
 
 #%%
 # with takes care of connection closing, and error handling already!
@@ -24,16 +24,24 @@ with conn:
 #%%
 
 # read vampire data from vamps.py
-with open(vamps.py, 'rb') as file:
+with open("vamps.py", 'rb') as file:
     exec(file.read())
 
+# read connections data from connections.py
+with open("connections.py", 'rb') as file:
+    exec(file.read())
 
-# generate vampire db
+# generate sqlite DBs
 with conn:
     cur = conn.cursor()
+    # generate vampires db
     cur.execute("DROP TABLE IF EXISTS Vampires")
     cur.execute("CREATE TABLE Vampires(Id INTEGER PRIMARY KEY, Nickname TEXT, Firstname TEXT, Lastname TEXT, Clan TEXT, Covenant  TEXT, Bloodline TEXT, Misc TEXT)")
     cur.executemany("INSERT INTO Vampires VALUES(?, ?, ?, ?, ?, ?, ?, ?)", vamps)
+    # generate connections db
+    cur.execute("DROP TABLE IF EXISTS Connections")
+    cur.execute("CREATE TABLE Connections(from_id INTEGER, to_id INTEGER, bi_directional INTEGER, connection_type TEXT, connection_details TEXT, start_date TEXT, end_date TEXT, misc TEXT)")
+    cur.executemany("INSERT INTO Connections VALUES(?, ?, ?, ?, ?, ?, ?, ?)", connections)
 
 #%%
 

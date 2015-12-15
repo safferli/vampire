ed8fa45344b457b33c3ed72ecd392d72c3f8e4a1
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-12-15 16:56:22 +0100

    rename files for more clarity

---
+++ dusseldorf.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+#%%
+import sqlite3 as lite
+# import sys
+import os
+
+#%%
+os.getcwd()
+os.chdir("D:\\github\\vampire")
+
+conn = lite.connect("vampires_dus.db")
+
+#%%
+# with takes care of connection closing, and error handling already!
+# with also auto-commits changes to the db
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT SQLITE_VERSION()")
+    data = cur.fetchone()
+
+    print("SQLite version: %s" % data)
+
+#%%
+
+# read vampire data from vamps.py
+with open("vampires.py", 'rb') as file:
+    exec(file.read())
+
+# read connections data from connections.py
+with open("connections.py", 'rb') as file:
+    exec(file.read())
+
+# generate sqlite DBs
+with conn:
+    cur = conn.cursor()
+    # generate vampires db
+    cur.execute("DROP TABLE IF EXISTS Vampires")
+    cur.execute("CREATE TABLE Vampires(Id INTEGER PRIMARY KEY, Nickname TEXT, Firstname TEXT, Lastname TEXT, Clan TEXT, Covenant  TEXT, Bloodline TEXT, Misc TEXT)")
+    cur.executemany("INSERT INTO Vampires VALUES(?, ?, ?, ?, ?, ?, ?, ?)", vamps)
+    # generate connections db
+    cur.execute("DROP TABLE IF EXISTS Connections")
+    cur.execute("CREATE TABLE Connections(from_id INTEGER, to_id INTEGER, bi_directional INTEGER, connection_type TEXT, connection_details TEXT, start_date TEXT, end_date TEXT, misc TEXT)")
+    cur.executemany("INSERT INTO Connections VALUES(?, ?, ?, ?, ?, ?, ?, ?)", connections)
+
+#%%
+
+# examine db
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Vampires")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+# http://www.pythoncentral.io/introduction-to-sqlite-in-python/
+# http://zetcode.com/db/sqlitepythontutorial/ (python2)
--- vampires.py
+++ vampires.py
@@ -1,57 +1,48 @@
-# -*- coding: utf-8 -*-
-
-#%%
-import sqlite3 as lite
-# import sys
-import os
-
-#%%
-os.getcwd()
-os.chdir("D:\\github\\vampire")
-
-conn = lite.connect("vampires_dus.db")
-
-#%%
-# with takes care of connection closing, and error handling already!
-# with also auto-commits changes to the db
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT SQLITE_VERSION()")
-    data = cur.fetchone()
-
-    print("SQLite version: %s" % data)
-
-#%%
-
-# read vampire data from vamps.py
-with open("vamps.py", 'rb') as file:
-    exec(file.read())
-
-# read connections data from connections.py
-with open("connections.py", 'rb') as file:
-    exec(file.read())
-
-# generate sqlite DBs
-with conn:
-    cur = conn.cursor()
-    # generate vampires db
-    cur.execute("DROP TABLE IF EXISTS Vampires")
-    cur.execute("CREATE TABLE Vampires(Id INTEGER PRIMARY KEY, Nickname TEXT, Firstname TEXT, Lastname TEXT, Clan TEXT, Covenant  TEXT, Bloodline TEXT, Misc TEXT)")
-    cur.executemany("INSERT INTO Vampires VALUES(?, ?, ?, ?, ?, ?, ?, ?)", vamps)
-    # generate connections db
-    cur.execute("DROP TABLE IF EXISTS Connections")
-    cur.execute("CREATE TABLE Connections(from_id INTEGER, to_id INTEGER, bi_directional INTEGER, connection_type TEXT, connection_details TEXT, start_date TEXT, end_date TEXT, misc TEXT)")
-    cur.executemany("INSERT INTO Connections VALUES(?, ?, ?, ?, ?, ?, ?, ?)", connections)
-
-#%%
-
-# examine db
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Vampires")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-# http://www.pythoncentral.io/introduction-to-sqlite-in-python/
-# http://zetcode.com/db/sqlitepythontutorial/ (python2)
+# list of vampires
+# (8): ID, nickname, firstnames, lastname, clan, covenant, bloodline, misc
+vamps = [
+    # PCs are single-digit (hopefully...)
+    (1, None, "Hauke", "Hinnrichs", "Nosferatu", "Ordo Dracul", None, None),
+    (2, None, "Valeria", "von Vyborg", "Ventrue", "Invictus", None, None),
+    (3, "Capt'n Ahab", "Sven", "Unterberg", "Mekhet", "Ordo Dracul", None, None),
+    (4, "Fred", "Thomas", "Du Bois", "Gangrel", None, None, None),
+    (5, None, None, None, None, None, None, None),
+    (6, None, None, None, None, None, None, None),
+    (7, None, None, None, None, None, None, None),
+    (8, None, None, None, None, None, None, None),
+    (9, None, None, None, None, None, None, None),
+    # NPCs start here, IDs are auto-incremented
+    (None, None, "Lucretia", None, "Mekhet", "Ordo Dracul", None, None),
+    (None, None, "Niklaus", None, None, "Ordo Dracul", None, None),
+    (None, None, "Werner", "Rosenberg", "Daeva", "Ordo Dracul", None, None),
+    (None, None, "Sebastian", "Radlov", "Gangrel", "Kartianer", None, None),
+    (None, None, "Baron Otto", "von Ulrich", "Mekhet", "Ordo Dracul", None, None),
+    (None, None, "Stephen", "Welsh", "Nosferatu", "Ordo Dracul", None, None),
+    (None, None, "Mara", None, None, "Ordo Dracul", None, None),# Kommandantin der Eidgeschworenen der Axt
+    (None, "Uhrmacher", None, None, None, "Ordo Dracul", None, None),
+    (None, "Henri", None, None, "Daeva", "Invictus", None, None),
+    (None, None, "Elise", None, None, None, None, None),
+    (None, "Donut", "Freddy", None, None, None, None, None),
+    (None, "Mutter Ey", None, None, "Daeva", None, None, None),
+    (None, "Wolfi", None, None, "Gangrel", None, None, None),
+    (None, "Biene", None, None, "Gangrel", None, None, None),
+    (None, None, "Alois", "Reynard", "Ventrue", "Invictus", None, None),
+    (None, "Ratte", None, None, "Gangrel", "Invictus", None, None),# Princeps of Gangrel
+    (None, None, None, "Matsumoto", None, None, "Kufukuji", None),
+    (None, "Architekt", "Hektor", None, None, "Ordo Dracul", None, None),
+    (None, "Der Philosoph", None, None, None, "Ordo Dracul", None, None),# Kogaion
+    (None, None, "Asmodius", "Schneider", None, "Ordo Dracul", None, None),
+    (None, None, "Markus", "Sommerfeld", "Ventrue", "Kartianer", None, None),
+    (None, None, "Artur", "Braunberg", "Daeva", "Kartianer", None, None),
+    (None, None, "Noach", None, "Nosferatu", None, None, None),
+    (None, None, "Dr Frederik", "Austein", None, "Kartianer", None, None),
+    (None, None, "Hamish", None, None, "Invictus", None, None),
+    (None, None, "Amalia", "Ziller", "Ventrue", "Invictus", None, None),
+    (None, None, "Anja", None, None, "Lancea Sancta", None, None),
+    (None, None, "Maria", "Malgrab", "Daeva", None, None, None),
+    (None, None, "Adrian", "von Langenfelden", None, "Kartianer", None, None),
+    (None, None, None, "Weitenau", None, None, None, None),
+    (None, None, None, "Brauner", None, None, None, None),
+    (None, "Vampir von Düsseldorf", "Peter", "Kürten", None, None, None, None)
+    #(None, None, "", "", "", None, None, None)
+    ]
